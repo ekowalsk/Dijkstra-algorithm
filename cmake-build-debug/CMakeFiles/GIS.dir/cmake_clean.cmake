17976f20f39d3c1cb654c40873fda3649a69bc68
ekowalsk <ewcia.kowalska@gmail.com>
2019-11-21 15:16:17 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GIS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GIS.dir/main.cpp.o"
+  "GIS.pdb"
+  "GIS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/Edge.h
@@ -0,0 +1,4 @@
+#ifndef GIS_EDGE_H
+#define GIS_EDGE_H
+
+#endif //GIS_EDGE_H
