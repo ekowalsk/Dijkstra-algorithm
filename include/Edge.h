b4b3d5191e7caef66aefe2c01f0447387c3b8c83
ekowalsk <ewcia.kowalska@gmail.com>
2019-12-02 22:47:12 +0100

    dodano gettery i settery w klasie Edge

--- include/Edge.h
+++ include/Edge.h
@@ -6,7 +6,17 @@ private:
     int source;
     int destination;
     int weight;
+
 public:
     Edge (int source, int destination, int weight);
+
+    int getSource() const;
+    void setSource(int source);
+    int getDestination() const;
+    void setDestination(int destination);
+    int getWeight() const;
+    void setWeight(int weight);
+
+    friend class Graph;
 };
 #endif //GIS_EDGE_H
--- src/Edge.cpp
+++ src/Edge.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
 #include "Edge.h"
+
 Edge::Edge(int source, int destination, int weight){
     this->source = source;
     this->destination = destination;
     this->weight = weight;
+}
+
+int Edge::getSource() const {
+    return source;
+}
+
+void Edge::setSource(int source) {
+    Edge::source = source;
+}
+
+int Edge::getDestination() const {
+    return destination;
+}
+
+void Edge::setDestination(int destination) {
+    Edge::destination = destination;
+}
+
+int Edge::getWeight() const {
+    return weight;
+}
+
+void Edge::setWeight(int weight) {
+    Edge::weight = weight;
 }
