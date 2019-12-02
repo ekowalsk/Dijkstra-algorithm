0b96f0e916473fea16054916118591fe2f45c7c7
ekowalsk <ewcia.kowalska@gmail.com>
2019-12-02 22:47:52 +0100

    dodano klasę Graph

--- include/Graph.h
+++ include/Graph.h
@@ -1,4 +1,19 @@
 #ifndef GIS_GRAPH_H
 #define GIS_GRAPH_H
 
+#include <map>
+#include <list>
+
+#include "Edge.h"
+
+class Graph {
+public:
+    typedef std::multimap<int, Edge>::iterator edgeIterator;
+    Graph();
+    ~Graph();
+    friend class Dijkstra;
+private:
+    std::multimap<int, Edge> * edges; // pairs <startVertex, Edge>
+    std::pair<edgeIterator, edgeIterator> getOutgoingEdges (int vertex);
+};
 #endif //GIS_GRAPH_H
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,1 +1,22 @@
+#include <limits.h>
+#include <iostream>
+
 #include "Graph.h"
+#include "Parser.h"
+
+//TODO utworzyć klasę Dijkstra algorithm, dodać do niej updateDistance
+Graph::Graph(){
+    edges = Parser::parseStdin();
+}
+
+std::pair<Graph::edgeIterator, Graph::edgeIterator> Graph::getOutgoingEdges (int vertex) {
+    return edges->equal_range(vertex);
+}
+
+Graph::~Graph(){
+    delete edges;
+}
+
+
+
+
