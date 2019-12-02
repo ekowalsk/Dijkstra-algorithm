b2d91003351ff6baea0668168a86d18a6c98f2c7
ekowalsk <ewcia.kowalska@gmail.com>
2019-12-02 22:44:33 +0100

    zmieniono metodę parsowania

--- include/Parser.h
+++ include/Parser.h
@@ -1,10 +1,13 @@
 #ifndef GIS_PARSER_H
 #define GIS_PARSER_H
 
+#include <map>
+
 #include "Edge.h"
 
 class Parser {
 public:
-    Edge parseLine();
+    static std::multimap<int, Edge> * parseStdin();
 };
+
 #endif //GIS_PARSER_H
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -2,11 +2,17 @@
 
 #include "Parser.h"
 
-Edge Parser::parseLine(){
-    int source, destination, weight;
-    std::cin >> source;
-    std::cin >> destination;
-    std::cin >> weight;
-    return Edge(source, destination, weight);
+std::multimap<int, Edge> * Parser::parseStdin(){
+    int source = -1, destination = -1, weight = -1;
+    auto * parsedEdges = new std::multimap<int, Edge>{};
+    while (std::cin >> source){
+        if (std::cin >> destination >> weight)
+            parsedEdges->insert(std::make_pair(source, Edge(source, destination, weight)));
+        else {
+            std::cerr << "Bad input data format" << std::endl;
+            exit(-1);
+        }
+    }
+    return parsedEdges;
 }
 
