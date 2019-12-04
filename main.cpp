e911caac6c02f143d2a3ead1ff124c9ea1354ae2
ekowalsk <ewcia.kowalska@gmail.com>
2019-12-04 15:45:07 +0100

    wykonanie algorytmu Dijksty

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "Parser.h"
+#include "Dijkstra.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::multimap<int, Edge> * graphEdges;
+    if (argc == 2){
+        graphEdges = Parser::parseFile(argv[1]);
+    }
+    else
+        graphEdges = Parser::parseStdin();
+    Graph graph = Graph(graphEdges);
+    graph.print();
+
+    Dijkstra dijkstra_algorithm = Dijkstra(graph);
+    dijkstra_algorithm.shortest_path(2,5);
+
     return 0;
 }
