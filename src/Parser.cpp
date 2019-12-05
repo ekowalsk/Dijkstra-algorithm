c7e271d134dcfa9559b3bdead98fd1d945b871cc
ekowalsk <ewcia.kowalska@gmail.com>
2019-12-05 14:08:33 +0100

    poprawiono formatowanie kodu

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -9,9 +9,8 @@ std::multimap<int, Edge> * Parser::parseStdin(){
     int source = -1, destination = -1, weight = -1;
     auto * parsedEdges = new std::multimap<int, Edge>{};
     while (std::cin >> source){
-        if (std::cin >> destination >> weight) {
+        if (std::cin >> destination >> weight)
             parsedEdges->insert(std::make_pair(source, Edge(source, destination, weight)));
-        }
         else {
             std::cerr << "Bad input data format" << std::endl;
             exit(-1);
