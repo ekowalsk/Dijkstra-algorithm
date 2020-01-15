817d6772d7679a7b1b20c8ed1b386ffd16b8e133
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-15 21:19:09 +0100

    dodano obsługę generowanych grafów

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "Parser.h"
 #include "Dijkstra.h"
-
+#include "GraphGenerator.h"
 
 int main(int argc, char* argv[]) {
     std::unordered_map<int, std::list<Edge>> * graphEdges;
@@ -13,12 +13,17 @@ int main(int argc, char* argv[]) {
         graphEdges = Parser::parseStdin();
     Graph graph = Graph(graphEdges);
 
-    Dijkstra dijkstra_algorithm = Dijkstra(graph);
-    dijkstra_algorithm.shortest_path(2);
-    dijkstra_algorithm.getPath(2, 7);
-    dijkstra_algorithm.getPath(2, 5);
-    dijkstra_algorithm.getPath(2, 1);
-    dijkstra_algorithm.getPath(1, 6);
+    //dijkstra_algorithm.shortest_path(2);
+    //dijkstra_algorithm.getPath(2, 7);
+    //dijkstra_algorithm.getPath(2, 5);
+    //dijkstra_algorithm.getPath(2, 1);
+
+    Graph::edgeMap * edges = GraphGenerator::generateGraph(1000, 3, 10);
+    Parser::parseGraphToFile(edges, "parsedEdges.txt");
+
+    Graph g2 = Graph(edges);
+    Dijkstra dijkstra_algorithm = Dijkstra(g2);
+    dijkstra_algorithm.shortest_path(1,60);
 
     return 0;
 }
