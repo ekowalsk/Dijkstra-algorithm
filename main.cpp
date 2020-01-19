f79a44f9d0724bb68d01efb746b109321d2b21e2
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-19 22:36:02 +0100

    poprawa funkcji main

--- main.cpp
+++ main.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-#include <chrono>
 
 #include "Parser.h"
 #include "Dijkstra.h"
+#include "Utils.h"
+#include "GraphGenerator.h"
 
 int main(int argc, char* argv[]) {
+    //Graph::edgeMap * edges = GraphGenerator::generateGraph(5000, 1, 1, 10);
+    //Parser::parseGraphToFile(edges, "parsedEdges.txt");
     std::unordered_map<int, std::list<Edge>> * graphEdges;
     Graph * graph = nullptr;
     Dijkstra * dijkstraAlgorithm = nullptr;
-    std::chrono::time_point<std::chrono::steady_clock> start, end;
 
-    if (argc >= 3) {
+    if (argc == 3 || argc == 4) {
         graphEdges = Parser::parseFile(argv[1]);
         graph = new Graph(graphEdges);
         dijkstraAlgorithm = new Dijkstra(*graph);
-        if (argc == 3){
-            start = std::chrono::steady_clock::now();
-            dijkstraAlgorithm->shortest_path(atoi(argv[2]));
-            end = std::chrono::steady_clock::now();
-        }
-        else if (argc == 4){
-            start = std::chrono::steady_clock::now();
-            dijkstraAlgorithm->shortest_path(atoi(argv[2]), atoi(argv[3]));
-            end = std::chrono::steady_clock::now();
-        }
-        double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
-        std::cout << "czas obliczeń: " << elapsed_seconds <<  " sekund" << '\n';
-    }
+        if (argc == 3)
+            dijkstraAlgorithm->shortestPath(atoi(argv[2]));
+        else
+            dijkstraAlgorithm->shortestPath(atoi(argv[2]), atoi(argv[3]));
+    } else
+        displayHelp();
     return 0;
 }
