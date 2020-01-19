ae376374a551fd38d0c7accb7bcf540f02ce0300
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-19 11:53:00 +0100

    dodano obsługę argc, argv

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
+#include <chrono>
 
 #include "Parser.h"
 #include "Dijkstra.h"
-#include "GraphGenerator.h"
 
 int main(int argc, char* argv[]) {
     std::unordered_map<int, std::list<Edge>> * graphEdges;
+    Graph * graph = nullptr;
+    Dijkstra * dijkstraAlgorithm = nullptr;
+    std::chrono::time_point<std::chrono::steady_clock> start, end;
 
-    if (argc == 2)
+    if (argc >= 3) {
         graphEdges = Parser::parseFile(argv[1]);
-    else
-        graphEdges = Parser::parseStdin();
-    Graph graph = Graph(graphEdges);
-
-    //dijkstra_algorithm.shortest_path(2);
-    //dijkstra_algorithm.getPath(2, 7);
-    //dijkstra_algorithm.getPath(2, 5);
-    //dijkstra_algorithm.getPath(2, 1);
-
-    Graph::edgeMap * edges = GraphGenerator::generateGraph(1000, 3, 10);
-    Parser::parseGraphToFile(edges, "parsedEdges.txt");
-
-    Graph g2 = Graph(edges);
-    Dijkstra dijkstra_algorithm = Dijkstra(g2);
-    dijkstra_algorithm.shortest_path(1,60);
-
+        graph = new Graph(graphEdges);
+        dijkstraAlgorithm = new Dijkstra(*graph);
+        if (argc == 3){
+            start = std::chrono::steady_clock::now();
+            dijkstraAlgorithm->shortest_path(atoi(argv[2]));
+            end = std::chrono::steady_clock::now();
+        }
+        else if (argc == 4){
+            start = std::chrono::steady_clock::now();
+            dijkstraAlgorithm->shortest_path(atoi(argv[2]), atoi(argv[3]));
+            end = std::chrono::steady_clock::now();
+        }
+        double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+        std::cout << "czas obliczeń: " << elapsed_seconds <<  " sekund" << '\n';
+    }
     return 0;
 }
